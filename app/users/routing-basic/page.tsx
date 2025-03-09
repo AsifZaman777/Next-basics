90c141bd9280739c34f6122da0dbce8d507124f2
AsifZaman777 <eramasif16@gmail.com>
2025-03-09 14:50:14 +0600

    [ROUTING] routing basics with external components

---
+++ app/users/routing-basic/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserTable from './components/UserTable'
+
+const page = async () => {
+  return (
+    <div>Users table
+      <UserTable />
+    </div>
+  )
+}
+
+export default page
