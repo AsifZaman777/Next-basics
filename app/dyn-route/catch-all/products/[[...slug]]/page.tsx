57ac445693a698657c33d59fc9dfef7f6e76cb43
AsifZaman777 <eramasif16@gmail.com>
2025-03-11 10:25:45 +0600

    [Routing] catch-all using slug

---
+++ app/dyn-route/catch-all/products/[[...slug]]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface Props
+{
+    params: {
+        slug: string[]; //accept an array of strings
+    };
+}
+
+const ProductPage = ({params:{slug}}:Props) => {
+    return (
+        <div>
+            <h1 className='font-bold'>Product Page</h1>
+            <p>{slug}</p>
+        </div>
+    );
+};
+
+export default ProductPage;
